97a6653274e58acf08360a2e47e2ecaf7db094b8
Krzysztof <lbkrpaw87@gmail.com>
2021-05-24 14:37:48 +0200

    angular 11 example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GuiGridModule } from '@generic-ui/ngx-grid';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GuiGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
